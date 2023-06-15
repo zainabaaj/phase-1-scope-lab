c0aa40a969ca57be966fd7cc0ffe6e4f99b423f5
Zeyna Baaj <zainabaaj@gmail.com>
2023-06-15 19:58:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Write your solution in this file!
+var customerName = 'bob';
+
+function upperCaseCustomerName() {
+    if (customerName) {
+        customerName = customerName.toUpperCase();
+        return customerName;
+    } else {
+        return "Customer name is not defined.";
+    }
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+    return bestCustomer;
+}
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob';
+    return bestCustomer;
+}
+
+
+function changeLeastFavoriteCustomer() {
+    throw new Error("Assignment to constant variable.");
+}
